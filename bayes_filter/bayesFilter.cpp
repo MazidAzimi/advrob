313e38d8147f326b2ebe3327cd67c689f0492f61
mazidazimi <mazid_azimi@hotmail.de>
2018-08-15 01:18:06 +1000

    almost done

--- bayes_filter/bayesFilter.cpp
+++ bayes_filter/bayesFilter.cpp
@@ -68,7 +68,20 @@ template<typename A> double vectorsum(vector<A> arg) {
 double motionProb( int prePosition, int curPosition, bool control ) {
 	double prob = 0;
 	/* QUESTION 3 HERE */
-
+    if (control==true){
+        if ((curPosition-prePosition == 1)||(curPosition ==1) && (prePosition == 20) )
+            prob = motionMoveProb[0];
+        else if ((curPosition-prePosition == 2)||(curPosition ==2) && (prePosition == 20)||(curPosition ==1) && (prePosition == 19) )
+            prob = motionMoveProb[1];
+        else
+            prob = 0;
+    }
+    else {
+        if (curPosition==prePosition)
+            prob = motionStayProb[0];
+        else
+            prob = motionStayProb[1];
+    }
 
 	return prob;
 }
@@ -77,8 +90,23 @@ double motionProb( int prePosition, int curPosition, bool control ) {
 /* Calculate observation probability */
 double obsProb( bool obser, int position ) {
 	double prob = 0;
-	bool isDoor;
+    bool isDoor;
+
 	/* QUESTION 4 HERE */
+
+    if ((position == 3) || (position == 5) || (position == 8)){
+        if (obser == true)
+            prob = obsDoorProb[0];
+        else
+            prob = obsDoorProb[1];
+    }
+     else {
+        if (obser == true)
+            prob = obsNotDoorProb[1];
+        else
+            prob = obsNotDoorProb[0];
+
+    }
 
 
 	return prob;
@@ -86,15 +114,41 @@ double obsProb( bool obser, int position ) {
 
 /* Update our belief */
 vector<double> bayesFilter( vector<double> preBelief, bool control, bool observation ) {
-	vector<double> curBelief;
+    vector<double> curBelief;
+    vector<double> currBelief;
+
+    double normalizer = 0;
+
 	curBelief.resize( preBelief.size() );
+    currBelief.resize( preBelief.size() );
 	/* QUESTION 5 HERE */
 	/* Hint: First implement the prediction step (Equation (3) in the notes) */
 	/* 	 Then implement the update step (Equation (4) in the notes) */
 
+    int num = curBelief.size();
 
+    //  PREDICTION
+    for (int k = 0; k<num; k++){
+        curBelief[k] = 0;
+        for (int i = 0; i<num; i++){
+        double motion_prob =  motionProb(i+1,k+1,control);
+        curBelief[k] +=  preBelief[k] * motion_prob;
+        }
+    }
 
-	return curBelief;
+    //  UPDATING
+    for (int k = 0; k<num; k++){
+        currBelief[k] = 0;
+        for (int i = 0; i<num; i++){
+
+            normalizer += obsProb(observation,i+1)*curBelief[i];
+
+        }
+        currBelief[k] = (1./normalizer)*obsProb(observation,k+1)*curBelief[k];
+
+    }
+
+    return curBelief;
 
 }
 
@@ -110,6 +164,10 @@ int main(int argc, char *argv[]) {
 	initBelief.resize( num );
 	// initialize the prior belief
 	/* QUESTION 1 & 2 HERE */
+
+    for(int i = 0; i<num; i++)
+        initBelief[i]=1./20;
+
 
 
 
@@ -129,7 +187,7 @@ int main(int argc, char *argv[]) {
 	 * 		motionStayProb */
 
 	/* Testing your motionProb(...) function */
-	int prePosition = 3, curPosition = 5;
+    int prePosition = 3, curPosition = 5;
 	bool control = true;
 	double mProb = motionProb( prePosition, curPosition, control );
 	cout << "************  Q3 **************************\n";
@@ -147,8 +205,8 @@ int main(int argc, char *argv[]) {
 	 * 		Observation probability is given in isDoorProb
 	 * 		and noDoorProb */
 	/* Testing your obsProb(...) function */
-	bool observation = true;
-	int position = 4;
+    bool observation = true;
+    int position = 5;
 	double oProb = obsProb( observation, position );
 	cout << "************  Q4 **************************\n";
 	cout << "The observation probability is " << oProb << "\n";
@@ -173,6 +231,35 @@ int main(int argc, char *argv[]) {
 	/* Just call the bayesFilter(...) function 4 times according to the given
 	   sequence of control inputs and observations */
 	/* HINT: Look at Q5 Testing code */
+    vector<double> curBelief1, curBelief2, curBelief3, curBelief4;
+    curBelief1 = bayesFilter( initBelief, 0, 1);
+    curBelief2 = bayesFilter( initBelief, 1, 0);
+    curBelief3 = bayesFilter( initBelief, 1, 1);
+    curBelief4 = bayesFilter( initBelief, 1, 1);
+
+    cout << "************  Q6 **************************\n";
+    cout << "Updated probabilities of curBelief1: \n";
+    printvector( curBelief1 );
+    cout << endl;
+
+    cout << "Updated probabilities of curBelief2: \n";
+    printvector( curBelief2 );
+    cout << endl;
+
+    cout << "Updated probabilities of curBelief3: \n";
+    printvector( curBelief3 );
+    cout << endl;
+
+    cout << "Updated probabilities of curBelief4: \n";
+    printvector( curBelief4 );
+    cout << endl;
+
+    double sum = 0;
+
+    for(int i; i<20; i++){
+        sum += curBelief1[i];
+    }
+    cout << sum<< endl;
 
 	return(0);
 }
