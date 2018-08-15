276f85eb29b11d62e4c2f0c3e31a80b56d851b9d
mazidazimi <mazid_azimi@hotmail.de>
2018-08-15 13:34:04 +1000

    add catkin

---
+++ catkin_ws/src/pf_localization/build/CMakeFiles/pf_localization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pf_localization.dir/src/pf_localization.cpp.o"
+  "devel/lib/pf_localization/pf_localization.pdb"
+  "devel/lib/pf_localization/pf_localization"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pf_localization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/src/pf_localization/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/user/Documents/advrob/pf_localization/build/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/usr/lib/x86_64-linux-gnu/qt4/bin:/usr/bin:/home/user/bin:/home/user/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bini"
+export PWD="/home/user/Documents/advrob/pf_localization/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/user/Documents/advrob/pf_localization/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/user/Documents/advrob/pf_localization:$ROS_PACKAGE_PATH"
